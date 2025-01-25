0d52a98cbd4607b8a74c103d1085a377225b4603
Garrett <99564508+Ilphu@users.noreply.github.com>
2025-01-25 14:16:44 -0500

    it works!

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,24 @@
 #include <SFML/Graphics.hpp>
 
-int main()
-{
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
-    window.setFramerateLimit(144);
+using namespace std;
+using namespace sf;
 
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-            {
+int main() {
+    auto window = RenderWindow(VideoMode({1920u, 1080u}), "Test");
+    window.setFramerateLimit(60);
+
+    CircleShape shape(100.f);
+    shape.setFillColor(Color::Green);
+
+    while (window.isOpen()) {
+        while (const optional event = window.pollEvent()) {
+            if (event->is<Event::Closed>()) {
                 window.close();
             }
         }
 
-        window.clear();
+        window.RenderTarget::clear(Color(100, 100, 100, 255));
+        window.draw(shape);
         window.display();
     }
 }
